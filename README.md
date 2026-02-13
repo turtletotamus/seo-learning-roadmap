d25f0ddfeac2e7af0ffef59a9f163661a106b246
kindaidiot0303 <kindaidiot0303@github.com>
2026-02-13 16:59:07 +0800

    Move SEO app to repo root

---
+++ README.md
@@ -0,0 +1,3 @@
+# SEO Learning Roadmap
+
+test
---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'raymond-plan-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
